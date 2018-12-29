3c74cbb4468833daf6a950db050997892b3fb38a
Bernard Huff <bernard.huff008@gmail.com>
2018-12-28 23:29:28 -0600

    Sweep

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-        <h1> Welcome to the swagg shop. </h1>
+        <h1> Welcome to the Swagg Shop. </h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
