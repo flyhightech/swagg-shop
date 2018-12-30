7e60958dcb349eeae3b507d29d517ab915e9763b
Bernard Huff <bernard.huff008@gmail.com>
2018-12-29 21:22:12 -0600

    Sweep,

---
+++ src/wishlist/wishlist.js
@@ -0,0 +1,13 @@
+import React, {
+    Component
+} from 'react';
+import './wishlist.css';
+
+class WishList extends Component {
+    render() {
+        return ();
+
+    }
+}
+
+export default WishList;
